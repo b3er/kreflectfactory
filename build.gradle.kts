8eeabb9c3d21226c9cc952019f48f3010f5ab3cf
Ilya Usanov <b3er@who.ms>
2022-02-03 16:24:25 +0300

    Jvm target 1.8 for jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,12 @@ tasks.test {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "11"
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 group = "com.github.b3er.kreflectfactory"
